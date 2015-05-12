dd59768ecfd83847bca0a94ffa364f9dc3256306
dgoncha7 <dgoncha7@students.d125.org>
2015-05-12 15:22:57 -0500

    Added board sample

--- MLGPics/src/mlgpics/MLGPics.java
+++ MLGPics/src/mlgpics/MLGPics.java
@@ -15,7 +15,6 @@ public class MLGPics {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("DANK MAYMAYS!");
-        System.out.println("Swag");
+        
     }
 }
